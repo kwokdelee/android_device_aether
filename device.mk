7770bffbe344cc3710040652cb6d9d6fb72f9187
Captain Throwback <captainthrowback@hotmail.com>
2022-05-21 17:45:42 -0400

    Move TARGET_OTA_ASSERT_DEVICE to device.mk
    Change-Id: I109f5f818ae6b87c8b5327cffa5e500abe9bc958

--- device.mk
+++ device.mk
@@ -25,3 +25,7 @@ $(call inherit-product, $(COMMON_PATH)/common.mk)
 
 # Copy modules for depmod
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*.ko,$(DEVICE_PATH)/prebuilt,$(TARGET_COPY_OUT_RECOVERY)/root/vendor/lib/modules/1.1)
+
+# Custom ROM asserts
+TARGET_OTA_ASSERT_DEVICE := I003D,ZS661KS
+
